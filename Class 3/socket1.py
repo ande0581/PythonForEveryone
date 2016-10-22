e5aac9423e44ab550ae49608dbe17e587a864dc6
janderson <jeffrey.d.anderson@gmail.com>
2016-10-22 18:26:07 -0500

    changing url i am querying from cnn to py4inf

--- Class 3/socket1.py
+++ Class 3/socket1.py
@@ -1,10 +1,10 @@
 import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#mysock.connect(('www.py4inf.com', 80))
-mysock.connect(('cnn.com', 80))
-#mysock.send('GET http://data.pr4e.org/intro-short.txt HTTP/1.0\n\n')
-mysock.send('GET http://www.cnn.com HTTP/1.0\n\n')
+mysock.connect(('www.py4inf.com', 80))
+#mysock.connect(('cnn.com', 80))
+mysock.send('GET http://data.pr4e.org/intro-short.txt HTTP/1.0\n\n')
+#mysock.send('GET http://www.cnn.com HTTP/1.0\n\n')
 
 while True:
     data = mysock.recv(512)
