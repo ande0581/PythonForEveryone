5b763adc36bf5e6104da8c99168e0029e2b21831
janderson <jeffrey.d.anderson@gmail.com>
2016-10-22 18:11:21 -0500

    initial commit

---
+++ Class 3/class3_week2_challenge.py
@@ -0,0 +1,12 @@
+"""
+Optional: Just for Fun
+
+There are a number of different ways to approach this problem.
+While we don't recommend trying to write the most compact code possible, it can sometimes be a fun exercise.
+Here is a a redacted version of two-line version of this program using list comprehension:
+"""
+
+"""
+import re
+print sum( [ ****** *** * in **********('[0-9]+',**************************.read()) ] )
+"""
